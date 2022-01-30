8eb53ce9a76e98df2f41ed598ee209a49acd8650
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-01-30 14:07:18 +0530

    added files pipeline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Home from './components/home/Home'
+import Navbar from './components/navbar/Navbar';
+import Portfolio from './components/portfolio/Portfolio';
+import About from './components/about/About';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Vaibhav Portfolio</h1>
+      <Navbar />
+      <div className="sections">
+        <Home />
+        <About />
+        <Portfolio />
+        <Experience />
+        <Contact />
+      </div>
     </div>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './about.css';
+
+const about = () => {
+    return <div className='about'>
+
+    </div>;
+};
+
+export default about;
