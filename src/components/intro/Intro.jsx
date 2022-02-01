8d34f7fdc057cd3f6d5c9e912bbd5d79b887b0e4
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-02-02 03:47:48 +0530

    intro done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './intro.scss';
+import { KeyboardArrowDownSharp } from '@material-ui/icons';
+import { init } from 'ityped'
 
 const Intro = () => {
+
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backSpeed: 60,
+            backDelay: 1500,
+            strings: ['Student', 'Frontend Developer', 'Backend Enthusiast', 'Learner']
+        })
+    }, [])
+
     return (
         <div className='intro' id='intro'>
-            <h2>Intro</h2>
+            <div className="left">
+                <div className="imgContainer">
+                    {/* <img src="assets/vaibhav_profile.jpg" alt="mishu" /> */}
+                    <img src="assets/vaibhavPhoto.jpeg" alt="mishu" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Vaibhav Mishra</h1>
+                    <h3>I'm a <span ref={textRef}></span></h3>
+                </div>
+                <a href="#about">
+                    <KeyboardArrowDownSharp className='arrow-icon' />
+                </a>
+            </div>
         </div>
     )
 };
