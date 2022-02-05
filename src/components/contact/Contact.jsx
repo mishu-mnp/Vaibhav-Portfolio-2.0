fb0723db31bd9f2121afeae1c26161d777d5037e
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-02-06 02:16:53 +0530

    contact done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.scss';
 
 const Contact = () => {
+
+    const [message, setMessage] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault()         // page should not reload
+        setMessage(true)
+    }
     return <div className='contact' id='contact'>
-        <h2>Contact</h2>
+        <div className="left">
+            <img src="assets/contact.jpg" alt="contact-me" />
+        </div>
+        <div className="right">
+            <h2>Contact</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="email" id="email" placeholder='Email' />
+                <textarea id='message' placeholder='Message...'></textarea>
+                <button type='submit'>Send</button>
+                {message && <span>Thanks! to contact I'll reply ASAP :) </span>}
+            </form>
+        </div>
     </div>;
 };
 
